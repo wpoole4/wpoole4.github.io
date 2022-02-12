3735cd6e659ba1b5dab3e8c34ce41c042a248873
wpoole4 <wpoole4@uncc.edu>
2022-02-12 18:35:26 -0500

    Forms

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,3 +1,9 @@
 function scriptTest(){
     alert("Hey my script is running");
 }
+function greeting(){
+    var name = doucment.getElementById("name").value;
+    var feeling = document.getElementById("feeling").value;
+    var welcome = "White Parrots welcomes you, " + name + ", we're glad/saddened to hear you're doing" + feeling + "!";
+    document.getElementById("greetingmessage").innerHTML = welcome;
+}
