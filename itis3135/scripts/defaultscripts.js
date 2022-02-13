1503a2f15f6929ca81fc8f09ff68e74f75b3b95d
wpoole4 <wpoole4@uncc.edu>
2022-02-12 20:40:08 -0500

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -2,7 +2,7 @@ function scriptTest(){
     alert("Hey my script is running");
 }
 function greeting(){
-    let name = doucment.getElementById("name").value;
+    let name = document.getElementById("name").value;
     let feeling = document.getElementById("feeling").value;
     let welcome = "White Parrots welcomes you, " + name + ", we're glad/saddened to hear you're doing" + feeling + "!";
     document.getElementById("greetingmessage").innerHTML = welcome.value;
