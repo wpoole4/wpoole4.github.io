c72d06a6e296462a36d0d344c244465b5aaffb16
wpoole4 <wpoole4@uncc.edu>
2022-02-13 16:05:45 -0500

    Button

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -4,6 +4,12 @@ function scriptTest(){
 function greeting(){
     let name = document.getElementById('name').value;
     let feeling = document.getElementById('feeling').value;
-    let welcome = "White Parrots welcomes you, " + name + ", we're glad/saddened to hear you're doing" + feeling + "!";
+    let welcome = "White Parrots welcomes you, " + name + ", we're glad/saddened to hear you're doing " + feeling + "!";
     document.getElementById("greetingmessage").innerHTML = welcome;
+}
+
+function outputParrot(){
+    let uInput = prompt("Type a word for a parrot to say");
+    uInput = uInput.toString;
+    alert(`Parrot says ${uInput}`);
 }
