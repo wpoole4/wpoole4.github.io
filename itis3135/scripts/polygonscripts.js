9eeaf4af581b23df1f394d7d232a21f9d101ada7
wpoole4 <wpoole4@uncc.edu>
2022-02-21 01:36:37 -0500

    Update polygonscripts.js

--- itis3135/scripts/polygonscripts.js
+++ itis3135/scripts/polygonscripts.js
@@ -1,7 +1,50 @@
-function getNumSides(){ //Function to take user input, ensure it's valid, and return it
-    let userInput = prompt("The White Parrots would like your number... must be between 1-10, we will name your polygon");
-    userInput = parseInt(userInput); //Prompt user and take input
+function getNumSides(){ //Function to take user input, ensure it's valid, and give a polygon
+    let polygonSides = prompt("The White Parrots would like your number... must be between 1-10, we will name your polygon");
+    polygonSides = parseInt(userInput); //Prompt user and take input
+    let validSides = validateEntry(polygonSides); //Validae the user input
+    let polygonName = getShape(validSides); //Get the polygon based off of the valid sides
+    alert(polygonName); //Put polygon in alert box
+}
 
+function getShape(side){ //Function tot get the polygon name 
+    var polygon;
+    //10 if statements to get the polygon
+    if (side == 0){
+        polygon = "Doesn't exist";
+    }
+    if (side == 1){
+        polygon = "Henagon";
+    }
+    if (side == 2){
+        polygon = "Digon";
+    }
+    if (side == 3){
+        polygon = "Trigon";
+    }
+    if (side == 4){
+        polygon = "Tetragon";
+    }
+    if (side == 5){
+        polygon = "Pentagon";
+    }
+    if (side == 6){
+        polygon = "Hexagon";
+    }
+    if (side == 7){
+        polygon = "Heptagon";
+    }
+    if (side == 8){
+        polygon = "Octagon";
+    }
+    if (side == 9){
+        polygon = "Enneagon";
+    }
+    if (side == 10){
+        polygon = "Decagon";
+    }
+    return polygon;
+}
+function validateEntry(userInput){ //Function to ensure that the user input is valid
     while (userInput > 10 || userInput < -10 || isNaN(userInput)){ //If input isn't a number or out of range, produce error until correct
         userInput = prompt("The White Parrots demand your number is real and between 0-10");
         userInput = parseInt(userInput);
