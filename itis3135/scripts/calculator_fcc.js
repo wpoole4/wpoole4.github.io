66e9d9a485384c992b892b26cd322051dc73613a
wpoole4 <wpoole4@uncc.edu>
2022-02-28 02:59:11 -0500

    Update calculator_fcc.js

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -1,4 +1,32 @@
-function showNum(num){
-    let number = num;
-    document.getElementById("result").value += number;
+window.onload = function()
+{
+    const calculator = document.querySelector('.calculator');
+    const keys = calculator.querySelector('.calculator__keys');
+
+    keys.addEventListener('click', e => {
+        if (e.target.matches('button')){
+            const key = e.target;
+            const action = key.dataset.action;
+
+            if (!action){
+                console.log('number key!');
+            }
+            if (action === 'add' ||
+                action === 'subtract' ||
+                action === 'multiply' ||
+                action == 'divide') {
+                    console.log('operator key!');
+                }
+            if (action === 'decimal'){
+                console.log('decimal key!');
+            }
+            if (action === 'clear') {
+                console.log('clear key!');
+            }
+            if (action === 'calculate'){
+                console.log('equal key!');
+            }
+            
+        }
+    })
 }
