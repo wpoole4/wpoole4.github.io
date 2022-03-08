70db707693f1a95e487ca08dbbc284f16016b8ab
wpoole4 <wpoole4@uncc.edu>
2022-03-08 04:33:10 -0500

    Array Done

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,21 +1,28 @@
+window.onload = function(){
+    document.getElementById('employees').focus();
+}
 const people = [];
 const salaries = [];
 
 function addSalary(){
+    document.getElementById('employees').focus();
     var selectedEmployee = document.getElementById('employees').value;
     var inputSalary = parseFloat(document.getElementById('salary').value);
 
     for (i = 0; i < people.length; i++){
         if (selectedEmployee == people[i]){
             alert("Choose a different employee, you've already added this one!");
+            return;
         }
     }
     if (inputSalary < 0 || isNaN(inputSalary)){
         alert("Please enter a positive number for a salary.");
+        return;
     } else {
         people.push(selectedEmployee);
         salaries.push(inputSalary);
     }
+    document.getElementById('employees').focus();
 }
 
 function displayResults(){
@@ -28,13 +35,14 @@ for (i = 0;  i < salaries.length; i++){
     sum += salaries[i];
 }
 average = sum / salaries.length;
-}
+
 
 for (i = 0; i < salaries.length; i++){
     if (salaries[i] > highestSalary){
         highestSalary = salaries[i];
         employeeHighestSalary = people[i];
     }
+}
 let headingMessage = "Results"
 document.getElementById("resultsHeading").innerHTML = headingMessage;
 let message = "The average salary is $" + average + " and the highest salary is " + employeeHighestSalary + " at $" + highestSalary;
@@ -42,5 +50,10 @@ document.getElementById("resultMessage").innerHTML = message;
 }
 
 function displaySalary(){
-
+    var table;
+    table = "<tr><th>Employee</th><th>Salary</th></tr>";
+    for (i = 0; i < salaries.length; i++){
+        table += "<tr><td>" + people[i] + "</td><td>" + salaries[i] + "</td></tr>";
+    }
+    document.getElementById("results_table").innerHTML = table;
 }
