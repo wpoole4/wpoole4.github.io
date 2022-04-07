4f9e1938aa237eee69e498a5fac19b83a8c7b17f
wpoole4 <wpoole4@uncc.edu>
2022-04-07 17:46:19 -0400

    Update Activity10.js

--- itis3135/scripts/Activity10.js
+++ itis3135/scripts/Activity10.js
@@ -2,14 +2,31 @@ $(document).ready(function() {
 
 
     // preload the image for each link
+    $("#image_list a").each(function() {
+
+
     
     // set up the event handlers for each link
+    let imgCaption = $(this).attr("title");
+    let images = new Image();
+    images.src = $(this).attr("href");
+       });
+       $("#images_list a").click(function(event) {
+           let imgUrl = $(this).attr("href");
 
-            // get the image URL and caption for each image and animate the caption
+           $("#image").fadeOut(500, function() {
+               $("#image").attr("src", imgUrl).fadeIn(500);
+           });
+           $("#caption").fadeout(500, function(){
+               $("#caption").text(imgCaption).fadeIn(500);
+           });
 
-        // cancel the default action of each link
-
-
+           // cancel the default action of each link
+           event.preventDefault();
+       });
 // move the focus to the first link
-
+$("li:first-child a").focus();
 }); // end ready
+
+
+//SOURCES USED: Textbook AND W3Schools
