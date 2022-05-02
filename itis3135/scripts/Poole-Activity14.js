f2042c2142222a70617ce2bece2b267dd2eac97f
wpoole4 <wpoole4@uncc.edu>
2022-05-02 02:28:24 -0400

    Activity 14 fixed

--- itis3135/scripts/Poole-Activity14.js
+++ itis3135/scripts/Poole-Activity14.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#nav_list li").click(function (){
         let name = $(this).children("a").attr("title");
-        $get(name + ".json", function(data, status){
+        $.get(name + ".json", function(data, status){
             let info = $.parseJSON(file);
             $("main > h1").html(data.speakers[0].title);
             $("main > h2").html(data.speakers[0].month);
