b82bc7e83206341a3e2a24a93ed7b81ec4447991
wpoole4 <wpoole4@uncc.edu>
2022-05-02 02:51:38 -0400

    Update Poole-Activity14.js

--- itis3135/scripts/Poole-Activity14.js
+++ itis3135/scripts/Poole-Activity14.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
     $("#nav_list li").click(function (){
         let name = $(this).children("a").attr("title");
-        $.get(name + ".json", function(data, status){
-            let info = $.parseJSON(file);
+        $.get(name + ".json")
+        $.ajax( {
+
+            url: file,
+            dataType: "Text",
+            success: function(file){
+
+            
             $("main > h1").html(data.speakers[0].title);
             $("main > h2").html(data.speakers[0].month);
             $("main > h3").html(data.speakers[0].speaker);
             $("main > img").attr("src", data.speakers[0].image);
             $("main > p").html(data.speakers[0].text);
+        }
         });
     });
-}); // end ready
+    });
+     // end ready
