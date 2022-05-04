81dce650ca67cf5dcafccf17167ea30fd02ce0e0
wpoole4 <wpoole4@uncc.edu>
2022-05-04 03:47:53 -0400

    Activity 15 done

--- itis3135/scripts/Poole-Activity15.js
+++ itis3135/scripts/Poole-Activity15.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+    $.getJSON("facultyList.json", function (data) {
+        $.each(data,function() {
+            $("#faculty").append(
+                "<img src= '" + value.image + "'><br>" +
+                "<h2" + value.full_name + "</h2>" +
+                "<h3>" + value.department + "</h3>" +
+                "<p>" + value.bio + "</p>"
+            );
+        });
+    });
 });
