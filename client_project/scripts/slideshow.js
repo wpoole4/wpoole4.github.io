99d639cca750942163275dc984f5537df0ffc708
wpoole4 <wpoole4@uncc.edu>
2022-05-06 00:53:45 -0400

    JQuery Slider

--- client_project/scripts/slideshow.js
+++ client_project/scripts/slideshow.js
@@ -1,36 +1,17 @@
 $(document).ready(function() {
-    var nextSlide = $("#slides img:first-child");
-    var nextCaption;
-    var nextSlideSource;
-    // start slide show
-    setInterval(
-        function() {
-            $("#slide").fadeOut(3000,
-                function() {
-                    if (nextSlide.next().length == 0) {
-                        nextSlide = $("#slides img:first-child");
-                    }
-                    else {
-                        nextSlide = nextSlide.next();
-                    }
-                    nextSlideSource = nextSlide.attr("src");
-                    nextCaption = nextSlide.attr("alt");
-                    $("#slide").attr("src",nextSlideSource).fadeIn(3000);
-                })
-        },
-    3000);
-})
+    $("#slider").bxSlider({
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 300,
+        slideMargin: 20,
+        randomStart: true,
+        movdeSlides: 1,
+        pause: 3000,
+        pagerType: 'short',
+        pagerSelector: '#id_pager',
+        pager: true,
+        captions: true
 
-//start slide show
-var timer1 = setInterval(runSlideShow, 5000);
-
-//starting and stopping the slideshow
-
-$("#slide").toggle(
-    function() {
-        clearInterval(timer1);
-    },
-    function() {
-        timer1 = setInterval(runSlideShow, 5000);
-    }
-)
+    });
+});
